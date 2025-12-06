29cbc517ce9727bd04d3f69cc3f259f7df43562a
Michael Rees Pullen <mipullen@microsoft.com>
2025-12-06 14:19:12 -0500

    Uncomment DMA buffer count and length in I2SAudioSpeaker setup for proper configuration

--- components/m5atoms3_audio/speaker/i2s_audio_speaker.cpp
+++ components/m5atoms3_audio/speaker/i2s_audio_speaker.cpp
@@ -21,13 +21,13 @@ void I2SAudioSpeaker::setup() {
   ESP_LOGI(TAG, "setup");
   auto cfg = M5.Speaker.config();
   cfg.task_priority = 10;
-  //cfg.dma_buf_count = this->dma_buf_count_;
-  //cfg.dma_buf_len = this->buffer_size_;
+  cfg.dma_buf_count = this->dma_buf_count_;
+  cfg.dma_buf_len = this->buffer_size_;
   //cfg.use_dac = true;         // Enable internal DAC
   //cfg.magnification = 12;      // Set gain to 1 (or higher
   //cfg.dma_buf_count = this->dma_buf_count_;
   //cfg.dma_buf_len = this->buffer_size_;
-  //cfg.sample_rate = this->sample_rate_;
+  cfg.sample_rate = this->sample_rate_;
   
   M5.Speaker.config(cfg);
   
